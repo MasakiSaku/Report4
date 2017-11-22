70fe68aa6664f70322fcd9cdc7245885ef75a844
Masaki Sakugawa <e175728@ie.u-ryukyu.ac.jp>
2017-11-22 22:07:55 +0900

    Exception Handling

--- src/report4.java
+++ src/report4.java
@@ -2,6 +2,13 @@ public class report4 {
     public static void main(String[] args){
 
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生したよ");
+            System.out.println(e.getMessage());
+        }
+
     }
 }
