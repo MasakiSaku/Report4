31e52986604a094a84737e659e56ae406a09a344
Masaki Sakugawa <e175728@ie.u-ryukyu.ac.jp>
2017-11-22 22:20:05 +0900

    report4 step3

--- src/report4.java
+++ src/report4.java
@@ -1,14 +1,7 @@
 public class report4 {
     public static void main(String[] args){
 
-        String str = null;
-        try {
-            System.out.println(str.length());
-        }
-        catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生したよ");
-            System.out.println(e.getMessage());
-        }
-
+        String str = "3．14";
+        double value = Double.parseDouble(str);
     }
 }
